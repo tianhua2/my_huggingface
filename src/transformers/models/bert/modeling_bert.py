c8bce9f24765a28b40aba3578d4ca5f846187302
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-05-17 21:29:59 -0700

    Update modeling_bert.py

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -333,19 +333,20 @@ class BertSelfAttention(nn.Module):
             attention_scores = attention_scores + attention_mask
 
         # Normalize the attention scores to probabilities.
-        attention_probs = nn.functional.softmax(attention_scores, dim=-1)
+        #attention_probs = nn.functional.softmax(attention_scores, dim=-1)
         def my_softmax(x):
-            th = nn.Threshold(-8, 0)
+            th = nn.Threshold(-8, -100)
             a = th(x)
             maxes = torch.max(a, -1, keepdim=True)[0]
             x_exp = torch.exp(a - maxes)
             x_exp_sum = torch.sum(x_exp, -1, keepdim=True)
             output_custom = x_exp/x_exp_sum
             return output_custom
-        attention_probs[0][4] = my_softmax(attention_scores[0][4])
-        attention_probs[0][5] = my_softmax(attention_scores[0][5])
-        attention_probs[0][6] = my_softmax(attention_scores[0][6])
-        attention_probs[0][7] = my_softmax(attention_scores[0][7])
+        #attention_probs[0][4] = my_softmax(attention_scores[0][4])
+        #attention_probs[0][5] = my_softmax(attention_scores[0][5])
+        #attention_probs[0][6] = my_softmax(attention_scores[0][6])
+        #attention_probs[0][7] = my_softmax(attention_scores[0][7])
+        attention_probs = my_softmax(attention_scores)
         
         # This is actually dropping out entire tokens to attend to, which might
         # seem a bit unusual, but is taken from the original Transformer paper.
