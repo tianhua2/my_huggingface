9a1e83abd6bc164374853f3bfa9381e4ffa947a9
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-06-24 00:49:58 -0700

    Update modeling_llama.py

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -362,7 +362,7 @@ class LlamaAttention(nn.Module):
         
         past_key_value = getattr(self, "past_key_value", past_key_value)
         cos, sin = self.rotary_emb(value_states, position_ids)
-        #query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
         
         HADAMARD = False #self.config.output_attentions
         QUANTIZE = False #self.config.output_hidden_states
