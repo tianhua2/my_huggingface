12faeca0f62bbc3dafbe8ad09352f9a6ec8e2ec5
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-06-28 23:08:12 -0700

    Update modeling_llama.py

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -405,7 +405,7 @@ class LlamaAttention(nn.Module):
         if H2O:
             ### Heavy + Recent
             heavy_budget_ratio = 0.2
-            recent_budget_ratio = 0.2
+            recent_budget_ratio = 0.1
             heavy_budget = int(heavy_budget_ratio * attn_weights.shape[-1])
             recent_budget = int(recent_budget_ratio * attn_weights.shape[-1])
             if heavy_budget > 256:
