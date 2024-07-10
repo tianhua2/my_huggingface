cbc5877d84c484bfcbf29c0f3c90029a72b9d3fb
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-07-09 23:04:06 -0700

    Update modeling_llama.py

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -380,10 +380,10 @@ class LlamaAttention(nn.Module):
         #    old_token_begin = -256
         #    old_token_end = -128
         old_token_end = -1
-        old_token_begin = int(kv_seq_len * 0.2)
-        #old_token_begin = 32
-        REFRESH = False
-        KV_BITS=2
+        #old_token_begin = int(kv_seq_len * 0.2)
+        old_token_begin = 0
+        REFRESH = True
+        KV_BITS=4
         #if kv_seq_len % 128 == 0 and kv_seq_len != 0 and REFRESH:
         if REFRESH:
             key_states_refresh = matmul_hadU(key_states[:,:,old_token_begin:old_token_end,:])
@@ -530,7 +530,7 @@ class LlamaAttention(nn.Module):
             H2O = True
         else:
             H2O = False
-        H2O = True
+        H2O = False
         if H2O:
             ### Heavy + Recent
             heavy_budget_ratio = 0.05
