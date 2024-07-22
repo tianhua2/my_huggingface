ea957d63ceaf81d7ae8e5258d359253138454b12
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-07-21 22:58:04 -0700

    Update modeling_llama.py

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -388,7 +388,7 @@ class LlamaAttention(nn.Module):
             attn_weights_temp = attn_weights_temp + causal_mask
             attn_weights_temp = torch.max(attn_weights_temp, torch.tensor(torch.finfo(attn_weights_temp.dtype).min))
         
-        DYNQ=True
+        DYNQ=False
         HADAMARD = False
         
         KRON = True
@@ -668,7 +668,7 @@ class LlamaAttention(nn.Module):
             H2O = True
         else:
             H2O = False
-        H2O = True
+        H2O = False
         if H2O:
             ### Heavy + Recent
             heavy_budget_ratio = 0.11
@@ -696,15 +696,16 @@ class LlamaAttention(nn.Module):
             attn_weights[~mask_bottom] = torch.finfo(attn_weights.dtype).min
 
         #Quantize Softmax input
-        quant_attn = False
+        quant_attn = True
         attn_bit = 8
         if quant_attn:
             #attn_weights_quant = matmul_hadU(attn_weights)
             #print('hadamard attn ', attn_weights_quant)
+            print('attn ', attn_weights)
             attn_weights_quant, scale_attn_weights_list, zero_attn_weights_list = asym_quantize_and_pack_i4(attn_weights, bits=attn_bit)
-            #print('quant attn ', attn_weights_quant)
+            print('quant attn ', attn_weights_quant)
             attn_weights = unpack_i4_and_asym_dequantize(attn_weights_quant, scale_attn_weights_list, zero_attn_weights_list)
-            #print('dequant attn ', attn_weights)
+            print('dequant attn ', attn_weights)
             #attn_weights = matmul_hadUt(attn_weights_quant)
         
         # upcast attention to fp32
