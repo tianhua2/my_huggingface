ed9bc4caf3fac9e8ca65275f5f96b96274b24401
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-07-27 19:27:11 -0700

    Update hadamard.py

--- src/transformers/models/llama/hadamard.py
+++ src/transformers/models/llama/hadamard.py
@@ -191,7 +191,7 @@ def asym_quantize_and_pack_i4(x: torch.Tensor, bits: int):
 
     prob_one_zero = torch.tensor(1e-5).to(q)
     prob_zero_one = torch.tensor(1e-6).to(q)
-    q = rand_flip_bits(q, bits.to(q), prob_one_zero, prob_zero_one) 
+    q = rand_flip_bits(q, bits, prob_one_zero, prob_zero_one) 
     
     # pack int4
     #q = q.to(dtype=torch.uint8)
