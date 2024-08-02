d18868b3049cf27bc2e8a04bc1e13e9af2cd8efa
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-08-02 01:23:00 -0700

    Update modeling_bert.py

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -354,7 +354,8 @@ class BertSelfAttention(nn.Module):
             frac_part = input - int_part
             #res=torch.multiply(torch.pow(2, int_neg),torch.subtract(1, torch.multiply(frac_part, 0.5)))
             #res = torch.exp(int_part)*torch.exp(frac_part)
-            res = torch.pow(2, int_part)*(1.0+frac_part-frac_part**2/2+frac_part**3/6)
+            #res = torch.pow(2, int_part)*(1.0+frac_part-frac_part**2/2+frac_part**3/6)
+            res = torch.pow(2, int_part)*torch.pow(2, frac_part)
             #res = torch.exp(x-x_max)
             return res
 
