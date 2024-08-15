738b469e8a99e010102a0a0f9fc0d4e50cffc289
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-08-14 18:02:15 -0700

    Update modeling_llama.py

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -711,15 +711,15 @@ class LlamaAttention(nn.Module):
         coeff = torch.range(0, coeff_length-1)
         coeff = 1+self.config.score_coeff/(coeff_length-1)*coeff.to(tmp_sum)
         tmp_sum = tmp_sum*coeff
-        mask = tmp_sum[:,:,-1][-1] > 1
-        mask = mask.unsqueeze(1)
+        mask = (tmp_sum[:,:,-1] > 2).transpose(-1,-2)
         #if H2O:
         _, tmp_topk = tmp_sum.topk(k=heavy_budget-1, dim=-1)
         token_life = attn_weights.shape[-2]-tmp_topk
         tmp_topk = tmp_topk.sort().values
         mask = mask.expand(tmp_topk.shape)
         mask[:,:,0:-1] = False
-        tmp_topk[mask] = attn_weights.shape[-1] - recent_budget
+        if attn_weights.shape[-1] > recent_budget:
+            tmp_topk[mask] = attn_weights.shape[-1] - recent_budget
         zeros = torch.zeros_like(tmp_sum, dtype=torch.bool)
         mask_bottom = zeros.scatter(-1, tmp_topk, True).unsqueeze(2)
         mask_bottom = mask_bottom.expand(mask_bottom.shape[0], mask_bottom.shape[1], attn_weights.shape[-2], mask_bottom.shape[-1])
