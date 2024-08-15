5c9c01f61357042f43ac5ef49db79bbb98d4be9a
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-08-14 19:06:57 -0700

    Update modeling_llama.py

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -711,7 +711,7 @@ class LlamaAttention(nn.Module):
         coeff = torch.range(0, coeff_length-1)
         coeff = 1+self.config.score_coeff/(coeff_length-1)*coeff.to(tmp_sum)
         tmp_sum = tmp_sum*coeff
-        mask = tmp_sum[...,-1] > 0.5
+        mask = tmp_sum[...,-1] > 1
         mask = mask.unsqueeze(-1)
         #if H2O:
         _, tmp_topk = tmp_sum.topk(k=heavy_budget, dim=-1)
