0adc0df424e9b670ecc557c665fee68b5ef01c43
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-08-16 23:20:33 -0700

    Update modeling_opt.py

--- src/transformers/models/opt/modeling_opt.py
+++ src/transformers/models/opt/modeling_opt.py
@@ -506,15 +506,14 @@ class OPTAttention(nn.Module):
         H2O = self.config.H2O
         #if H2O:
         ### Heavy + Recent
-        if self.layer_idx < 16:
-            heavy_budget_ratio = self.config.heavy_budget_ratio+0.01
-            recent_budget_ratio = self.config.recent_budget_ratio+0.01
-        else:
-            heavy_budget_ratio = self.config.heavy_budget_ratio-0.01
-            recent_budget_ratio = self.config.recent_budget_ratio-0.01
-
-        #heavy_budget_ratio = self.config.heavy_budget_ratio
-        #recent_budget_ratio = self.config.recent_budget_ratio
+        #if self.layer_idx < 16:
+        #    heavy_budget_ratio = self.config.heavy_budget_ratio+0.01
+        #    recent_budget_ratio = self.config.recent_budget_ratio+0.01
+        #else:
+        #    heavy_budget_ratio = self.config.heavy_budget_ratio-0.01
+        #    recent_budget_ratio = self.config.recent_budget_ratio-0.01
+        heavy_budget_ratio = self.config.heavy_budget_ratio
+        recent_budget_ratio = self.config.recent_budget_ratio
             
         heavy_budget = int(heavy_budget_ratio * attn_weights.shape[-1])
         recent_budget = int(recent_budget_ratio * attn_weights.shape[-1])
