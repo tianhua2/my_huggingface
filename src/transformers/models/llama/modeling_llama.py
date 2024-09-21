69bae64561271653d0a7dd4b9bce94fa5dedb705
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-09-21 15:57:11 -0400

    Update modeling_llama.py

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -722,7 +722,10 @@ class LlamaAttention(nn.Module):
         #recent_budget = int(recent_budget_ratio * 120)
         # Heavy Hitter Mask (Based on global statistics)
         tmp_attn = nn.functional.softmax(attn_weights_temp, dim=-1, dtype=torch.float32).to(attn_weights.dtype)
-        tmp_attn = torch.round(tmp_attn*8)/8
+        #tmp_attn = torch.round(tmp_attn*8)/8
+        tmp_attn_exp = torch.round(torch.log2(x))+15
+        tmp_attn_exp_round = torch.round(tmp_attn_exp/2)*2-15
+        tmp_attn = 2** tmp_attn_exp_round
         tmp_sum = torch.sum(tmp_attn, dim=-2) 
            
         coeff_length = tmp_sum.shape[-1]
