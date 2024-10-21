ce7f90ebc20654699821f892f00578bbc88a05f6
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2024-10-21 00:35:19 -0400

    Update modeling_llama.py

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -761,9 +761,9 @@ class LlamaAttention(nn.Module):
         with open('/content/history.csv', 'a') as f:
             f.write(f"{percentage}\n")
         with open('/content/seq_length.csv', 'a') as f:
-            f.write(f"{str(tmp_attn.size()[0])}\n")
+            f.write(f"{str(tmp_attn.size()[-1])}\n")
         with open('/content/kept_length.csv', 'a') as f:
-            f.write(f"{str(tmp_topk.size()[0])}\n")    
+            f.write(f"{str(tmp_topk.size()[-1])}\n")    
         token_life = attn_weights.shape[-2]-tmp_topk
         #tmp_topk = tmp_topk.sort().values
         #mask = mask.expand(tmp_topk.shape)
